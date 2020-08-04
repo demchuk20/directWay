299c2e788a6977148a6e04814a58d44dbdda0d25
Юрій Демчук <yura.demchuk.20@gmail.com>
2020-08-04 17:11:21 +0300

    initial commit

---
+++ directWay-serv/src/main/java/com/directWay/DirectWayApplication.java
@@ -0,0 +1,13 @@
+package com.directWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectWayApplication.class, args);
+	}
+
+}
